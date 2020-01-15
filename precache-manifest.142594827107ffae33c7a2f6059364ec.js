226083d2b1245f3ab39181e5ec82c63613765f38
Derryk Boyd <derrykboyd@gmail.com>
2020-01-14 23:21:44 -0700

    Updates

---
+++ precache-manifest.142594827107ffae33c7a2f6059364ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab65b679ad0c6960b70711ca8e1daa44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2c637b8694ce35d4dd5",
+    "url": "/static/css/main.16cfc1ac.chunk.css"
+  },
+  {
+    "revision": "22ff7d4431c9911bf6db",
+    "url": "/static/js/2.591cae95.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.591cae95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2c637b8694ce35d4dd5",
+    "url": "/static/js/main.f98bf165.chunk.js"
+  },
+  {
+    "revision": "dd39fa749db70b460e69",
+    "url": "/static/js/runtime-main.ace1e775.js"
+  }
+]);
